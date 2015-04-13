9711d4a72617c06d90f2883b53f1cd676fbe2278
tyler goodman <tylercgoodman@gmail.com>
2015-04-13 14:47:27 -0700

    gulp sequential tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var runSequence = require('run-sequence');
 var protractor = require('gulp-protractor').protractor;
 
 
 gulp.task('mocha', function (done) {
+  process.env.DB = process.env.DB || 'test';
   gulp.src('./test/mocha/**/*.js')
     .pipe(mocha({
       colors: true,
@@ -13,4 +15,10 @@ gulp.task('mocha', function (done) {
     .once('end', done);
 });
 
-gulp.task('default', []);
+gulp.task('test', function (done) {
+  runSequence('mocha', done);
+});
+
+gulp.task('default', function (done) {
+  gulp.run('test', done);
+});
